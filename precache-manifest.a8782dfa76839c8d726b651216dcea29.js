2a1c9d66486a1f8fb69b7f1e2de32dcd397878eb
poveu <pawelloboziak@gmail.com>
2019-01-15 21:37:31 +0100

    Updates

---
+++ precache-manifest.a8782dfa76839c8d726b651216dcea29.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c04f575b245fb0e32ee",
+    "url": "/React-Magento-2-Product-View/static/js/runtime~main.7c04f575.js"
+  },
+  {
+    "revision": "30655260dcc6013ff4bd",
+    "url": "/React-Magento-2-Product-View/static/js/main.30655260.chunk.js"
+  },
+  {
+    "revision": "2ffeb023bbd1be7676ae",
+    "url": "/React-Magento-2-Product-View/static/js/1.2ffeb023.chunk.js"
+  },
+  {
+    "revision": "30655260dcc6013ff4bd",
+    "url": "/React-Magento-2-Product-View/static/css/main.cba2a549.chunk.css"
+  },
+  {
+    "revision": "ae234f5391a537fac860218d8807a4e3",
+    "url": "/React-Magento-2-Product-View/index.html"
+  }
+];
