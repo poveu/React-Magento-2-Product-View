cc3d1af699443769f53b7c9aa421fb8ceca5ce8f
poveu <pawelloboziak@gmail.com>
2019-01-15 21:41:39 +0100

    Updates

--- precache-manifest.ac9f1a330308b4638ba8b1e1517e66cf.js
+++ precache-manifest.ac9f1a330308b4638ba8b1e1517e66cf.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/React-Magento-2-Product-View/static/js/runtime~main.7c04f575.js"
   },
   {
-    "revision": "b3a014cc111312fd245f",
-    "url": "/React-Magento-2-Product-View/static/js/main.b3a014cc.chunk.js"
+    "revision": "33fbb88cf22f2a7cd7c2",
+    "url": "/React-Magento-2-Product-View/static/js/main.33fbb88c.chunk.js"
   },
   {
     "revision": "2ffeb023bbd1be7676ae",
     "url": "/React-Magento-2-Product-View/static/js/1.2ffeb023.chunk.js"
   },
   {
-    "revision": "b3a014cc111312fd245f",
+    "revision": "33fbb88cf22f2a7cd7c2",
     "url": "/React-Magento-2-Product-View/static/css/main.cba2a549.chunk.css"
   },
   {
-    "revision": "2f9805404e121323e1dd03dccbf190a0",
+    "revision": "b67f432c4af853b72124cf186174f611",
     "url": "/React-Magento-2-Product-View/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/React-Magento-2-Product-View/precache-manifest.4b904f336b4a6b6dc09c1fdf354fecfa.js"
+  "/React-Magento-2-Product-View/precache-manifest.ac9f1a330308b4638ba8b1e1517e66cf.js"
 );
 
 workbox.clientsClaim();
